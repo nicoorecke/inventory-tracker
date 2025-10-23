28b08e88203316d4bda5cc2d6168b5b191023364
Nicolas Agustin RECKE <nrecke@edenor.com>
2025-10-23 14:14:58 -0300

    soda stereo

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,7 +56,7 @@ def initialize_data(conn):
         VALUES
             -- Beverages
             ('Coca cola (500000ml)', 1.50, 115, 15, 0.80, 16, 'Hydrating bottled water'),
-            ('Soda (355ml)', 2.00, 93, 8, 1.20, 10, 'Carbonated soft drink'),
+            ('Soda ', 2.00, 93, 8, 1.20, 10, 'Carbonated soft drink'),
             ('Energy Drink (250ml)', 2.50, 12, 18, 1.50, 8, 'High-caffeine energy drink'),
             ('Coffee (hot, large)', 2.75, 11, 14, 1.80, 5, 'Freshly brewed hot coffee'),
             ('Juice (200ml)', 2.25, 11, 9, 1.30, 5, 'Fruit juice blend'),
